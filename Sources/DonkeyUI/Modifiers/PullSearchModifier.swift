d9713be0ec45bb3032d9784968dedaebed2e2917
Francisco Sainz <pacoswilliams@gmail.com>
2023-04-06 00:38:10 +0200

    nice cock

--- Sources/DonkeyUI/Modifiers/PullSearchModifier.swift
+++ Sources/DonkeyUI/Modifiers/PullSearchModifier.swift
@@ -98,15 +98,10 @@ public struct PullSearchModifier: ViewModifier {
                     .ignoresSafeArea()
             GeometryReader { proxy in
                 ScrollView {
-                    VStack(spacing: 0) {
-//                        Text("\(offsetY)")
-                            content
+                        content
                         .background(GeometryReader {
                             Color.clear.preference(key: ViewOffsetKey.self, value: -$0.frame(in: .named("scrollView")).minY)
                         })
-                        
-                        
-                    }
                 }
                 .coordinateSpace(name: "scrollView")
                 .onPreferenceChange(ViewOffsetKey.self) { offset in
