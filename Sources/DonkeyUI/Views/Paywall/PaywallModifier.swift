3861e3f5fbd03847388e92a949c8a1608c5a5746
Francisco Sainz <pacoswilliams@gmail.com>
2023-04-13 21:38:52 +0200

    niceeeeee

--- Sources/DonkeyUI/Views/Paywall/PaywallModifier.swift
+++ Sources/DonkeyUI/Views/Paywall/PaywallModifier.swift
@@ -9,33 +9,36 @@ import RevenueCat
 
 public struct PaywallModifier: ViewModifier {
     
-    public init(user: UserViewModel = UserViewModel.shared, views: [IdentifiableView], successAction: @escaping () -> Void, errorAction: @escaping (PublicError?, Bool) -> Void, privacyUrl: String) {
+    public init(user: UserViewModel = UserViewModel.shared, views: [IdentifiableView], successAction: @escaping () -> Void, onOpen: @escaping () -> Void, errorAction: @escaping (PublicError?, Bool) -> Void, privacyUrl: String) {
         self.user = user
         self.views = views
         self.successAction = successAction
         self.privacyUrl = privacyUrl
         self.errorAction = errorAction
+        self.onOpen = onOpen
     }
     
     @ObservedObject var user = UserViewModel.shared
     var views: [IdentifiableView]
     let privacyUrl: String
     var successAction: () -> Void
+    var onOpen: () -> Void
     var errorAction: (PublicError?, Bool) -> Void
+
     
     
     public func body(content: Content) -> some View {
         content
             .errorToast(presented: $user.showNetworkError)
             .fullScreenCover(isPresented: $user.paywallOn) {
-                PaywallView(views: views, successAction: successAction, errorAction: errorAction, proEntitlementId: UserViewModel.shared.etitlementId, privacyUrl: privacyUrl)
+                PaywallView(views: views, successAction: successAction, onOpen: onOpen, errorAction: errorAction, proEntitlementId: UserViewModel.shared.etitlementId, privacyUrl: privacyUrl)
             }
       }
 }
 
 public extension View {
-    func paywall(views: [IdentifiableView] = [], successAction: @escaping () -> Void, errorAction:  @escaping (PublicError?, Bool) -> Void, privacyUrl: String) -> some View {
-        modifier(PaywallModifier(views: views, successAction: successAction, errorAction: errorAction, privacyUrl: privacyUrl))
+    func paywall(views: [IdentifiableView] = [], successAction: @escaping () -> Void, onOpen: @escaping () -> Void, errorAction:  @escaping (PublicError?, Bool) -> Void, privacyUrl: String) -> some View {
+        modifier(PaywallModifier(views: views, successAction: successAction, onOpen: onOpen, errorAction: errorAction, privacyUrl: privacyUrl))
     }
 }
     
