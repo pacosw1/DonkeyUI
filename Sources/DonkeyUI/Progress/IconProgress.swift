e1206ba48a0f7cadaa02dd5ce793f355a115cdcc
Francisco Sainz <pacoswilliams@gmail.com>
2023-05-24 18:22:46 +0200

    comotefue

--- Sources/DonkeyUI/Progress/IconProgress.swift
+++ Sources/DonkeyUI/Progress/IconProgress.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 public struct ProgressIcon: View {
-    public init(progress: CGFloat, animationStart: CGFloat = 0, icon: String = "trophy.fill", iconSize: CGFloat = 40) {
+    public init(progress: CGFloat, icon: String = "trophy.fill", iconSize: CGFloat = 40) {
         self.progress = progress
-        self.animationStart = animationStart
         self.icon = icon
         self.iconSize = iconSize
+        self.offset = .zero
+        self.square = false
     }
     
     var progress: CGFloat
-    @State var animationStart: CGFloat = 0
+    @State var offset: Angle = .degrees(0)
     
     var icon: String = "trophy.fill"
     var iconSize: CGFloat = 40
+    var square: Bool
+    
     public var body: some View {
         VStack {
             GeometryReader { proxy in
@@ -31,10 +34,10 @@ public struct ProgressIcon: View {
                         .foregroundColor(.gray.opacity(0.15))
                         .animation(.spring(), value: progress)
 
-                    WaterWave(progress: progress, offset: animationStart)
+                    Wave(offset: offset, percent: progress)
                         .fill(gradientColor(color: .yellow))
-                        .frame(width: iconSize, height: iconSize, alignment: .center)
-
+                        .clipShape((Circle()).scale(0.92))
+//                        .frame(width: iconSize, height: iconSize, alignment: .center)
                         .mask {
                             Image(systemName: icon)
                                 .resizable()
@@ -42,11 +45,15 @@ public struct ProgressIcon: View {
 
                         }
                 }
-                .animation(.spring(), value: progress)
-                .frame(width: iconSize, height: iconSize, alignment: .center)
+//                .frame(width: iconSize, height: iconSize, alignment: .center)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                           self.offset = Angle(degrees: 360)
+                    }
+                }
             }
         }
-        .frame(width: iconSize, height: iconSize)
+//        .frame(width: iconSize, height: iconSize)
     }
 }
 
@@ -59,53 +66,54 @@ private func gradientColor(color: Color) -> LinearGradient {
 struct ProgressIcon_Previews: PreviewProvider {
     static var previews: some View {
         HStack(spacing: 30){
-            ProgressIcon(progress: 0.3, icon: "drop.fill", iconSize: 50)
-            ProgressIcon(progress: 0.5, icon: "circle.fill", iconSize: 50)
-            ProgressIcon(progress: 0.3, iconSize: 50)
+//            ProgressIcon(progress: 0.3, icon: "drop.fill", iconSize: 50)
+            ProgressIcon(progress: 0.5, icon: "clock.fill", iconSize: 200)
+                .padding()
+//            ProgressIcon(progress: 0.3, iconSize: 50)
 
         }
-        .padding(.horizontal)
-        .padding()
+//        .padding(.horizontal)
+//        .padding()
     }
 }
 
+struct Wave: Shape {
 
-struct WaterWave: Shape {
-    var progress: CGFloat
-    var waveHeight: CGFloat {
-        return 0.1
-    }
+    var offset: Angle
+    var percent: Double
     
-    var offset: CGFloat
-
-    var animatableData: AnimatablePair<CGFloat, CGFloat> {
-        get {
-           AnimatablePair(offset, progress)
-        }
-
-        set {
-            progress = (newValue.second)
-        }
+    var animatableData: Double {
+        get { offset.degrees }
+        set { offset = Angle(degrees: newValue) }
     }
     
     func path(in rect: CGRect) -> Path {
-        return Path { path in
-            path.move(to: .zero)
-            
-            let progressHeight: CGFloat = (1 - progress) * rect.height
-            let height: CGFloat = waveHeight * rect.height
-            
-            for value in stride(from: 0, through: rect.width, by: 2) {
-                let x: CGFloat = value
-                let sine: CGFloat = sin(Angle(degrees: value + offset).radians)
-                let y: CGFloat = progressHeight + (height * sine)
-                
-                path.addLine(to: CGPoint(x: x, y: y))
-            }
-            
-            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
-            path.addLine(to: CGPoint(x: 0, y: rect.height))
+        var p = Path()
 
+        // empirically determined values for wave to be seen
+        // at 0 and 100 percent
+        let lowfudge = 0.02
+        let highfudge = 0.98
+        
+        let newpercent = lowfudge + (highfudge - lowfudge) * percent
+        let waveHeight = 0.015 * rect.height
+        let yoffset = CGFloat(1 - newpercent) * (rect.height - 4 * waveHeight) + 2 * waveHeight
+        let startAngle = offset
+        let endAngle = offset + Angle(degrees: 360)
+        
+        p.move(to: CGPoint(x: 0, y: yoffset + waveHeight * CGFloat(sin(offset.radians))))
+        
+        for angle in stride(from: startAngle.degrees, through: endAngle.degrees, by: 5) {
+            let x = CGFloat((angle - startAngle.degrees) / 360) * rect.width
+            p.addLine(to: CGPoint(x: x, y: yoffset + waveHeight * CGFloat(sin(Angle(degrees: angle).radians))))
         }
+        
+        p.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        p.addLine(to: CGPoint(x: 0, y: rect.height))
+        p.closeSubpath()
+        
+        return p
     }
 }
+
+
